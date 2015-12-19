810ad6303a3da221f60eda7f63335fbce0e52c67
madhavan <madhavan.ekanathan@live.com.sg>
2015-12-19 15:11:00 +1300

    MYOB.EMP.Payslip.Test

---
+++ MYOB.EMP.Payslip.Test/UnitTestPayslip.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MYOB.EMP.Payslip;
+
+namespace MYOB.EMP.Payslip.Tests
+{
+   
+    [TestClass()]
+    public class UnitTestPayslip
+    {
+        EmpPayslip objPS = new EmpPayslip("Madhavan", "Ekanathan", 60050, 9, "01 March - 31 March");
+        
+        [TestMethod()]
+        public void CalculatedGrosspayValueTest()
+        {
+            Assert.AreEqual(5004, Math.Round(objPS.GrossIncome));
+        }
+        [TestMethod()]
+        public void CalculateIncomeTaxTest()
+        {
+            Assert.AreEqual(922, Math.Round(objPS.CalculateIncomeTax(objPS.AnnualSalary)));
+        }
+        [TestMethod()]
+        public void CalculatedIncomeTaxValueTest()
+        {
+            Assert.AreEqual(922, Math.Round(objPS.IncomeTax));
+        }
+        [TestMethod()]
+        public void CalculatedNetIncomeValueTest()
+        {   
+            Assert.AreEqual(4082, Math.Round(objPS.NetIncome));
+        }
+        [TestMethod()]
+        public void CalculatedSuperValueTest()
+        {   
+            Assert.AreEqual(450, Math.Round(objPS.Super));
+        }
+    }
+
+    
+}
+
+
---
+++ MYOB.EMP.Payslip.Test/UnitTestReadCSV.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MYOB.EMP.Payslip;
+
+namespace MYOB.EMP.Payslip.Tests
+{   
+   
+    [TestClass()]
+    public class UnitTestReadCSV
+    {
+        GetCSVFile objCSV = new GetCSVFile();
+
+        [TestMethod()]
+        public void GivenFileIsNotValidTest()
+        {   
+            //arrange         
+            objCSV.FileName = @"C:\Users\madha\Documents\Visual Studio 2015\Projects\MYOB.EMP.Payslip\MYOB.EMP.Payslip\Employee.xls";//p.ReadFile();
+            //act
+            var result = objCSV.FileCheck();
+            //assert
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod()]
+        public void GivenFileWithoutFileNameTest()
+        {
+            objCSV.FileName = @"C:\Users\madha\Documents\Visual Studio 2015\Projects\MYOB.EMP.Payslip\MYOB.EMP.Payslip\";//p.ReadFile();
+            var result = objCSV.FileCheck();
+            Assert.AreEqual(false, result);
+        }
+        [TestMethod()]
+        public void IsGivenFileNotCSVTest()
+        {                        
+            objCSV.FileName = @"C:\Users\madha\Downloads\CloudDesignPatternsBook-PDF.pdf"; // p.ReadFile();
+            var result = objCSV.FileCheck();
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod()]
+        public void IsGivenFileCSVTest()
+        {
+            objCSV.FileName = @"C:\Users\madha\Documents\Visual Studio 2015\Projects\MYOB.EMP.Payslip\MYOB.EMP.Payslip\Employee.csv"; // p.ReadFile();
+            var result = objCSV.FileCheck();
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod()]
+        public void CSVHasHeaderTest()
+        {            
+            objCSV.HeaderChar = "Y";
+            Assert.AreEqual(true, objCSV.CSVHasHeader());
+        }
+        [TestMethod()]
+        public void CSVHasNoHeaderTest()
+        {
+            objCSV.HeaderChar = "N";
+            Assert.AreEqual(false, objCSV.CSVHasHeader());
+        }
+        [TestMethod()]
+        public void CSVDataInFormatTest()
+        {
+            bool DataInFormat=objCSV.CSVDataInFormat("Madhavan,Ekanathan,60050,9%,01 March - 31 March");                       
+            Assert.AreEqual(true, DataInFormat);
+        }
+
+        [TestMethod()]
+        public void CSVDataNotInFormatTest()
+        {
+            //Arrange
+            string[] InCorrectPatterns = { "Madhavan,Ekanathan,60050,9,01 March - 31 March", //Super Rate missing %
+                                           ",Ekanatha,60050,9%,01 March - 31 March", //Missing First Name
+                                           "Madhavan,,60050,9%,01 March - 31 March", //Missing Last Name
+                                           "Madhavan,Ekanathan,,9%,01 March - 31 March", //Missing Annual Income
+                                           "Madhavan,Ekanathan,60050,,01 March - 31 March", //Missing Super Rate
+                                           "Madhavan,Ekanathan,60050,9%,", //Missing Pay Period
+                                           "Madhavan,Ekanathan,60050,9%,01 Mar - 31 Mar", //Pay Period Month in Short Format
+                                           "Madhavan,Ekanathan,60050,9%, - 31 Mar", //Pay Period Month has no start date
+                                           "Madhavan,Ekanathan,60050,9%,01 Mar -", //Pay Period Month has no end date
+                                           "Madhavan,Ekanathan,-60050,9%,01 Mar -", //Annual Income in Negative Value
+                                           ",,,,", //Empty Pattern
+                                           "" // Empty String
+                                         };
+            bool DataInFormat;
+            foreach (string line in InCorrectPatterns)
+            {
+                //Act
+                DataInFormat = objCSV.CSVDataInFormat(line); //super rate dont have %
+                //Assert
+                Assert.AreEqual(false, DataInFormat);
+            }
+            
+        }
+    }
+}
+
+
